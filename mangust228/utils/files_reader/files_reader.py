3b933ec683beba79d394a1a0d5077fd1dfa62ca8
mangustik228 <bacek.mangust@gmail.com>
2023-11-10 08:13:03 +0700

    feat: return content in get_files_paths

--- mangust228/utils/files_reader/files_reader.py
+++ mangust228/utils/files_reader/files_reader.py
@@ -1,12 +1,13 @@
 import os
 from collections import namedtuple
 from typing import Generator
+import lzma
 
 
-File = namedtuple("File", ["path", "params"])
+File = namedtuple("File", ["path", "params", "content"])
 
 
-def get_files_paths(base_path: str, file_format: str, sep: str = "_") -> Generator[File, None, None]:
+def get_files_paths(base_path: str, file_format: str, sep: str = "_", return_content=False) -> Generator[File, None, None]:
     '''
     Функция для итерации по файлам с вложенной древовидной структурой.
 
@@ -18,6 +19,9 @@ def get_files_paths(base_path: str, file_format: str, sep: str = "_") -> Generat
         расширение файлов, которые будут возвращены
     sep : str, optional
         Используемый разделитель параметров в наименовании файла,, by default "_"
+    return_content: bool
+        Можно возвращать сразу контент, если указан архив ".xz", то вернет уже разжатый
+
 
     Yields
     ------
@@ -25,7 +29,21 @@ def get_files_paths(base_path: str, file_format: str, sep: str = "_") -> Generat
         _description_
     '''
 
-    def get_info(fp: str, sep: str):
+    def get_content(fp: str):
+        if not return_content:
+            return
+        if file_format[-2:] == "xz":
+            with open(fp, mode="rb") as file:
+                bytes_data = file.read()
+            content = lzma.decompress(bytes_data)
+            return content.decode("utf-8")
+        with open(fp, "r") as file:
+            content = file.read()
+        return content
+
+    def get_info(fp: str):
+        if fp[-2:] == "xz":
+            params = fp.replace(".xz", "")
         params = fp.split("/")[-1].split(".")[0]
         params = params.replace("___", ".").replace("__", "/")
         result_params = []
@@ -34,11 +52,14 @@ def get_files_paths(base_path: str, file_format: str, sep: str = "_") -> Generat
                 result_params.append(int(param))
             else:
                 result_params.append(param)
-        return File(path=fp, params=result_params)
+
+        content = get_content(fp)
+
+        return File(path=fp, params=result_params, content=content)
 
     for path in os.listdir(base_path):
         current_path = f"{base_path}/{path}"
         if os.path.isdir(current_path):
-            yield from get_files_paths(current_path, file_format, sep)
+            yield from get_files_paths(current_path, file_format, sep, return_content)
         elif current_path[-len(file_format):] == file_format:
-            yield get_info(current_path, sep)
+            yield get_info(current_path)
--- tests/test_get_files/test_get_files.py
+++ tests/test_get_files/test_get_files.py
@@ -12,7 +12,6 @@ def test_get_files_paths():
 
 def test_get_files_paths_items():
     fp = "tests/src/get_files_2"
-    # www___example___com__this__is__super__page_90.html
     gen = get_files_paths(fp, ".html")
     item = next(gen)
     assert item.path == "tests/src/get_files_2/2023/November/10/7/www___example___com__page_90.html"
@@ -25,3 +24,19 @@ def test_get_files_paths_sep():
     item = next(gen)
     assert item.path == "tests/src/get_files_3/2023/November/10/7/www___example___com__page_3|90.html"
     assert item.params == ["www.example.com/page_3", 90]
+
+
+def test_get_content():
+    fp = "tests/src/get_files_3"
+    gen = get_files_paths(fp, ".html", "|", return_content=True)
+    item = next(gen)
+    assert item.content == "hello world"
+
+
+def test_get_content_xz():
+    fp = "tests/src/get_files_4"
+    gen = get_files_paths(fp, ".xz", return_content=True)
+    item = next(gen)
+    assert item.content == "hello world"
+    assert item.params == ["world"]
+    assert item.path == "tests/src/get_files_4/2023/world.html.xz"
